0e0b093a0e0a011dbae0b45e44df8e79ba6f0d14
D20CM <scottbrown500@hotmail.com>
2023-03-14 18:39:20 +0000

    refactor delete uncommented code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -191,7 +191,7 @@ function App() {
       }
 
       //the second condition in the lines below will skip scoring of (repeated) letters that have already been scored above
-
+      //skip scoring of letters already scored as repeated letters, otherwise score a letter
       for (let i = 0; i < letters.length; i++) {
         if (
           word.includes(letters[i]) &&
@@ -201,66 +201,12 @@ function App() {
         }
       }
 
-      // if (word.includes(letter1) && !repeatedLetters.includes(letter1)) {
-      //   score++;
-      // }
-      // if (word.includes(letter2) && !repeatedLetters.includes(letter2)) {
-      //   score++;
-      // }
-      // if (word.includes(letter3) && !repeatedLetters.includes(letter3)) {
-      //   score++;
-      // }
-      // if (word.includes(letter4) && !repeatedLetters.includes(letter4)) {
-      //   score++;
-      // }
-      // if (word.includes(letter5) && !repeatedLetters.includes(letter5)) {
-      //   score++;
-      // }
-      // if (word.includes(letter6) && !repeatedLetters.includes(letter6)) {
-      //   score++;
-      // }
-      // if (word.includes(letter7) && !repeatedLetters.includes(letter7)) {
-      //   score++;
-      // }
-      // if (word.includes(letter8) && !repeatedLetters.includes(letter8)) {
-      //   score++;
-      // }
-      // if (word.includes(letter9) && !repeatedLetters.includes(letter9)) {
-      //   score++;
-      // }
-
       return score;
     }
 
     setResults(results);
     setDisplayResults(true);
   }
-
-  // async function setDefinitions(results) {
-  //   results.forEach(async (result) => {
-  //     const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${result}?key=${process.env.REACT_APP_API_KEY}`;
-  //     const response = await fetch(url);
-  //     const wordInfo = await response.json();
-  //     const definition = wordInfo[0].shortdef;
-  //     console.log(definition);
-  //     return definition;
-  //   });
-  // }
-
-  // useEffect(() => {
-  //   console.log("Results are: " + results);
-  //   results.forEach(async (result, index) => {
-  //     const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${result}?key=${process.env.REACT_APP_API_KEY}`;
-  //     const response = await fetch(url);
-  //     const wordInfo = await response.json();
-  //     console.log(wordInfo);
-  //     const definition = `${result}: ` + wordInfo[0].shortdef;
-  //     setDefinitions([...definitions, definition]);
-  //     // console.log(definitions);
-  //   });
-  // }, [displayResults, results]);
-
-  // console.log(definitions);
 
   function resetGame() {
     setLetter1("");
